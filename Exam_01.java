102a1bb1f8d68c4e6f4c554813f4e1302ee09f4e
icici0093 <68003227+icici0093@users.noreply.github.com>
2021-01-05 18:01:52 +0900

    Delete Exam_01.java

--- Exam_01.java
+++
@@ -1,9 +0,0 @@
-
-public class Exam_01 {
-	//주석(한줄) 코드에 영향을 미치지 않는 내용을 작성 가능!
-	//BSD Style, K&R Style
-	//K&R Style
-	public static void main(String [] args) {
-		System.out.println("Test Hello");
-	}
-}
