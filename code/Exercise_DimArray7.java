84b591fb527ee02cc5f2aa1a16486f5837632e5a
icici0093 <68003227+icici0093@users.noreply.github.com>
2021-01-14 17:41:50 +0900

    Update Exercise_DimArray7.java

--- code/Exercise_DimArray7.java
+++ code/Exercise_DimArray7.java
@@ -2,6 +2,11 @@ package com.kh.dimarray;
 
 public class Exercise_DimArray7 {
 	public static void main(String[] args) {
+		// 5  6 15 16 25 
+		// 4  7 14 17 24 
+		// 3  8 13 18 23 
+		// 2  9 12 19 22 
+		// 1 10 11 20 21 
 		int[][] arrs = new int[5][5];
 		int k = 1;
 		for(int i = 0; i < arrs.length; i++) {
